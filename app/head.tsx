99b459e0e5ba67b51fc383c353ff1e64bab845bc
[Dev] YTAgenteAndroid <DavidSBrito075@gmail.com>
2026-02-26 17:43:55 -0300

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4315108909193125"
+        crossOrigin="anonymous"
+      ></script>
+    </>
+  )
+}
